f65afe366f1c05dad0eadd7329e335697911a15a
Kamqer <86482559+AleksanderBondar@users.noreply.github.com>
2021-12-05 00:20:50 +0100

    Nothing to see 👀

--- apps/flipbook/src/pages/index.tsx
+++ apps/flipbook/src/pages/index.tsx
@@ -1,12 +1,27 @@
-import styled from '@emotion/styled';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
-const Header = styled.div `
-    font-size: 48px;
-`
 
-const HomePage = () => (
-    <Header>Elo</Header>
-)
-
-export default HomePage;
+const App = () => {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        const app = document.getElementById('root');
+        ReactDOM.render(
+            <>
+                <script src="js/jquery.min.js"></script>
+                <script src="js/html2canvas.min.js"></script>
+                <script src="js/three.min.js"></script>
+                <script src="js/pdf.min.js"></script>
+                <script src="js/3dflipbook.min.js"></script>
+            </>,
+            app,
+        );
+    });
+    return (
+        <div>
+            <div id="root">COS</div>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+    );
+};
+export default App;
