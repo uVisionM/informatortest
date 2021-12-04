88d1b814ec01b8b87c70162f9a8573b206851e25
Kamqer <86482559+AleksanderBondar@users.noreply.github.com>
2021-12-04 22:07:10 +0100

    Deleting unused components 🔨

--- apps/flipbook/src/components/atoms/index.tsx
+++ apps/flipbook/src/components/atoms/index.tsx
@@ -1,2 +1,1 @@
-export * from './TestAtoms';
-export * from './CustomHelmet';
+export {};
--- apps/flipbook/src/pages/index.tsx
+++ apps/flipbook/src/pages/index.tsx
@@ -1,30 +1,12 @@
-import { Layout } from '@/src/layouts';
-import { LogoAexol } from '@/src/assets';
-import { TestMolecules } from '@/src/components';
-import aexol_logo from '@/public/images/aexol_logo.svg';
-import aexol_full_logo from '@/public/images/aexol_full_logo.png';
-import Link from 'next/link';
 import styled from '@emotion/styled';
+import React from 'react';
 
-const ALink = styled.div`
-    a {
-        color: black;
-    }
-`;
+const Header = styled.div `
+    font-size: 48px;
+`
 
-
-const HomePage = () => {
-    return (
-        <Layout pageTitle="HomePage">
-            <TestMolecules />
-            <LogoAexol />
-            <ALink>
-                <Link href="/posts/first">Dynamic Route Post Example</Link>
-            </ALink>
-            <img src={aexol_logo.src} alt="" />
-            <img src={aexol_full_logo.src} alt="" />
-        </Layout>
-    );
-};
+const HomePage = () => (
+    <Header>Elo</Header>
+)
 
 export default HomePage;
