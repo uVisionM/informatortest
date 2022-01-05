7950ad4e8811f2d4ba9ba56f431b11ff9154972f
Aleksander Bondar <86482559+AleksanderBondar@users.noreply.github.com>
2022-01-05 11:50:27 +0100

    AddPage, etc. functions
    :smile:

--- apps/flipbook/src/components/organism/flipbook.tsx
+++ apps/flipbook/src/components/organism/flipbook.tsx
@@ -2,6 +2,8 @@ import { PageFlip } from 'page-flip';
 import { useEffect } from 'react';
 import { Wrapper, Btn, LogoPB, Godlo } from '../../styles/styleBook';
 import { MarkdownContentPages }from '../../styles/styleMD';
+import { AddPage, AddPageImage } from '../atoms/AddPage';
+import { AddFrontPage, Front } from '../atoms/AddFrontPage';
 interface IFlipBook {
     pages: Array<
         | {
@@ -57,93 +59,38 @@ export const FlipBook: React.FC<IFlipBook> = ({ pages, test, graduate, science }
             minHeight: 300,
             maxHeight: 800,
             disableFlipByClick: true,
-          
         });
         pages.sort((a, b) => a?.changedToMatter.pageNumber - b?.changedToMatter.pageNumber);
-        let loc = document.getElementById('page-storage')
-        for (let i = 0; i < pages.length; i++)
-        {
-            if(i>=0)
-            {
-            let page = document.createElement('div');
-            let pageContent = document.createElement('div');
-            let pageText = document.createElement('div');
-            page.className = 'page';
-            pageContent.className = 'flex flex-col ml-4 w-[95%]';
-            pageText.innerHTML = pages[i]?.clean;
-            pageContent.appendChild(pageText);
-            page.appendChild(pageContent);
-            loc!.appendChild(page);
+        let loc = document.getElementById('page-storage');
+        for (let i = 0; i < pages.length; i++) {
+            if (i >= 0) {
+                AddPage('', 'flex flex-col ml-4 w-[95%]', '', pages[i]?.clean)
             }
         }
-        let page = document.createElement('div');
-        let pageContent = document.createElement('div');
-        let pageText = document.createElement('div');
-        page.className = 'page';
-        pageContent.className = 'bg-pb w-full h-full flex flex-col py-[49%]';
-        pageText.className = 'text-white text-4xl text-center';
-        pageText.innerHTML = "Nasi Absolwenci!"
-        pageContent.appendChild(pageText);
-        page.appendChild(pageContent);
-        loc!.appendChild(page);
-        graduate.map((g)=>{
-            let page = document.createElement('div');
-            let pageContent = document.createElement('div');
-            let pageText = document.createElement('div');
-            let pageImg = document.createElement('img');
-            page.className = 'page';
-            pageContent.className = 'flex flex-col ml-4 w-[95%]';
-            pageText.className = 'mt-4'
-            pageText.innerHTML = g!.clean
-            pageImg.className = 'w-96 h-auto mx-auto mt-4';
-            pageImg.src = g!.changedToMatter.image;
-            pageContent.appendChild(pageImg)
-            pageContent.appendChild(pageText);
-            page.appendChild(pageContent);
-            loc!.appendChild(page);
-        })
-        page = document.createElement('div');
-        pageContent = document.createElement('div');
-        pageText = document.createElement('div');
-        page.className = 'page';
-        pageContent.className = 'bg-pb w-full h-full flex flex-col py-[49%]';
-        pageText.className = 'text-white text-4xl text-center';
-        pageText.innerHTML = "Koła naukowe na naszej uczelni!"
-        pageContent.appendChild(pageText);
-        page.appendChild(pageContent);
-        loc!.appendChild(page);
-        science.map((g)=>{
-            for (let i = 0; i < g.cleaned.length; i++)
-            {
-                let page = document.createElement('div');
-                let pageContent = document.createElement('div');
-                let pageText = document.createElement('div');
-                let pageTextHeader = document.createElement('h1');
-                page.className = 'page';
-                pageContent.className = 'flex flex-col ml-4 w-[95%]';
-                pageTextHeader.className = 'flex justify-center';
-                pageTextHeader.innerHTML = g.changed[i].name;
-                pageText.innerHTML = g.cleaned[i]
-                pageContent.appendChild(pageTextHeader);
-                pageContent.appendChild(pageText);
-                page.appendChild(pageContent);
-                loc!.appendChild(page);
+        AddFrontPage('', 'bg-pb w-full h-full flex flex-col py-[49%]', 'text-white text-4xl text-center', 'Nasi Absolwenci', '')
+        graduate.map((g) => {
+            AddPageImage('', 'flex flex-col ml-4 w-[95%]', 'mt-4', g!.clean, 'w-96 h-auto mx-auto mt-4', g!.changedToMatter.image);
+        });
+        AddFrontPage('', 'bg-pb w-full h-full flex flex-col py-[49%]', 'text-white text-4xl text-center', 'Koła naukowe na naszej uczelni!','')
+        science.map((g) => {
+            for (let i = 0; i < g.cleaned.length; i++) {
+                AddPage('','flex flex-col ml-4 w-[95%]', 'flex justify-center', g.cleaned[i])
             }
-        })
-        pageFlip.on('init', () =>{
+        });
+        pageFlip.on('init', () => {
             loc = document.getElementById('page-total');
-            loc!.innerHTML = pageFlip.getPageCount().toString();   
-        })
+            loc!.innerHTML = pageFlip.getPageCount().toString();
+        });
         pageFlip.on('changeState', () => {
             loc = document.getElementById('page-current');
-            loc!.innerHTML = (pageFlip.getCurrentPageIndex()+1).toString();
+            loc!.innerHTML = (pageFlip.getCurrentPageIndex() + 1).toString();
         });
         let prev = document.getElementById('prev');
         prev?.addEventListener('click', () => {
-            pageFlip.turnToPrevPage()
+            pageFlip.turnToPrevPage();
             loc = document.getElementById('page-current');
             loc!.innerHTML = (pageFlip.getCurrentPageIndex() + 1).toString();
-        })
+        });
         let next = document.getElementById('next');
         next?.addEventListener('click', () => {
             if (pageFlip.getCurrentPageIndex() < pageFlip.getPageCount() - 2) {
@@ -151,8 +98,8 @@ export const FlipBook: React.FC<IFlipBook> = ({ pages, test, graduate, science }
             }
             loc = document.getElementById('page-current');
             loc!.innerHTML = (pageFlip.getCurrentPageIndex() + 1).toString();
-        })
-        pageFlip.loadFromHTML(document.querySelectorAll('.page')); 
+        });
+        pageFlip.loadFromHTML(document.querySelectorAll('.page'));
     });
     const logo = 'https://pb.edu.pl/wp-content/themes/pb/assets/img/logo-pb-w.png';
     const godlo = 'https://pb.edu.pl/wp-content/themes/pb/assets/img/godlo.png';
@@ -162,11 +109,7 @@ export const FlipBook: React.FC<IFlipBook> = ({ pages, test, graduate, science }
                 <MarkdownContentPages>
                     <div id="flipbook-container">
                         <div className="page page-cover" data-density="hard">
-                            <h1 className="flex flex-row">
-                                <Godlo src={godlo} />
-                                <div className="border-l-2 h-24 mt-[39%]"></div>
-                                <LogoPB src={logo} />
-                            </h1>
+                            <Front/>
                         </div>
 
                         <div id="page-storage"></div>
